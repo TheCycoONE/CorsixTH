6c98d7e1338be3e8379acd291baef1c48b6a47da
Alberth <alberth289346@gmail.com>
2016-03-06 09:14:05 +0100

    Introduce countCured function

--- CorsixTH/Lua/entities/patient.lua
+++ CorsixTH/Lua/entities/patient.lua
@@ -528,6 +528,18 @@ function Patient:tapFoot()
   end
 end
 
+-- Increments "cures" statistics
+function Patient:countCured()
+  local hosp = self.hospital
+  hosp.num_cured = hosp.num_cured + 1
+  hosp.num_cured_ty = hosp.num_cured_ty + 1
+  local casebook = hosp.disease_casebook[self.disease.id]
+  casebook.recoveries = casebook.recoveries + 1
+  if self.is_emergency then
+    hosp.emergency.cured_emergency_patients = hosp.emergency.cured_emergency_patients + 1
+  end
+end
+
 --! Make the patient leave the hospital. This function also handles some
 --! statistics (number of cured/kicked out patients, etc.)
 --! The mood icon is updated accordingly. Reputation is impacted accordingly.
@@ -556,13 +568,7 @@ function Patient:goHome(reason)
     if hosp.num_cured < 1 then
       self.world.ui.adviser:say(_A.information.first_cure)
     end
-    hosp.num_cured = hosp.num_cured + 1
-    hosp.num_cured_ty = hosp.num_cured_ty + 1
-    local casebook = hosp.disease_casebook[self.disease.id]
-    casebook.recoveries = casebook.recoveries + 1
-    if self.is_emergency then
-      hosp.emergency.cured_emergency_patients = hosp.emergency.cured_emergency_patients + 1
-    end
+    self:countCured()
   elseif reason == "kicked" then
     self:setMood("exit", "activate")
     if not self.is_debug then
